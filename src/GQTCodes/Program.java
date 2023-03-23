5c6f68a6ab0834c2c2b39b47f5cc895a0064a79c
ashish <ashish@HACKERSKABAAP>
2023-03-23 22:51:10 +0530

    First Commit

---
+++ src/GQTCodes/Program.java
@@ -0,0 +1,25 @@
+package GQTCodes;
+
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Value:");
+		int n = sc.nextInt();
+		
+	
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n;j++) {
+				System.out.print("$ ");
+				
+			}
+			for(int j=1;j<=n;j++) {
+				System.out.print("# ");
+				
+			}
+			System.out.println();
+		}
+	}
+
+}
